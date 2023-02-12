d3e552a919a5a657bff62c755db501c136df0ea5
Nettakrim <you@example.com>
2023-02-12 22:16:13 +0000

    improved villages

--- Assets/Scripts/Village.cs
+++ Assets/Scripts/Village.cs
@@ -7,12 +7,24 @@ public class Village : MonoBehaviour
     [SerializeField] private int minDamageInclusive;
     [SerializeField] private int maxDamageInclusive;
 
+    private bool collected;
+
+    private void Update() {
+        if (collected) {
+            float y = transform.GetChild(0).localScale.y;
+            if (y > 0.1) {
+                y-=Time.deltaTime*4*((y-0.1f)*1.111f);
+            }
+            transform.GetChild(0).localScale = new Vector3(1,y,1);
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             Island.current.collected = true;
             int heal = Random.Range(minDamageInclusive, maxDamageInclusive+1);
             Player.instance.pirateBand.AddPeople(heal);
-            Destroy(gameObject);
+            collected = true;
         }
     }
 }
